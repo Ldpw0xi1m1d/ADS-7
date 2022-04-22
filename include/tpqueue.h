2c5155d33ccf70d8bd0af12e742fa846522a5de5
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-04-22 23:29:26 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -7,14 +7,14 @@
 template <typename T>
       class TPQueue {
  private:
-    struct Item {
+           struct Item {
                   T value;
                   Item* next;
-                };
+           };
                 Item* head = nullptr;
                 TPQueue::Item* create(const T& value);
  public:
-      TPQueue();
+         TPQueue();
                 TPQueue(const T&);
                 void addHead(const T&);
                 void push(const T& value);
